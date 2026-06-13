eb6f7fe24465677e1e321d897f04c66a59ab2f84
sahilbhende25 <sahilbhende25@gmail.com>
2026-06-13 14:51:20 -0400

    [20260613]-POTD

---
+++ POTD/2130.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        vector<int>v;
+        while(head) {
+            v.push_back(head->val);
+            head = head->next;
+        }
+        int maxSum = INT_MIN;
+        for(int i = 0 ; i < v.size()/2 ; i++) {
+            maxSum = max(maxSum,v[i]+v[v.size()-1-i]);
+        }
+        return maxSum;
+    }
+};
