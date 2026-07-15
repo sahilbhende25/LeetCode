13d42b922bca3ae74586f04ea7806b05780afb39
sahilbhende25 <sahilbhende25@gmail.com>
2026-07-15 19:48:35 -0400

    [20260715]-POTD

---
+++ POTD/169.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        n--;
+        n/=2;
+        return nums[n];
+    }
+};
