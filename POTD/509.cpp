bdb33dcc10fc457e92ad365857bdd436f3f7da93
sahilbhende25 <sahilbhende25@gmail.com>
2026-07-23 13:35:09 -0400

    [20260723]-POTD

---
+++ POTD/509.cpp
@@ -0,0 +1,15 @@
+class Solution {
+private:
+    vector<int> dp = vector<int>(31, -1);
+public:
+    int fib(int n) {
+        if(n==0) {
+            return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = fib(n-1) + fib(n-2); 
+    }
+};
