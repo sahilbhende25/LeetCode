984ec9621ffaca79e38834abfa2f269dca8282d3
sahilbhende25 <sahilbhende25@gmail.com>
2026-07-25 23:12:33 -0400

    [20260725]-POTD

---
+++ POTD/303.cpp
@@ -0,0 +1,21 @@
+class NumArray {
+public:
+    vector<int>ref;
+    NumArray(vector<int>& nums) {
+        ref = nums;
+    }
+    
+    int sumRange(int left, int right) {
+        int sum = 0;
+        for(int i = left ; i <= right ; i++) {
+            sum += ref[i];
+        }
+        return sum;
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
