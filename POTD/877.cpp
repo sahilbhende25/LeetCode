86cb731116299e4ad24ecec611dab0b42bb8d95c
sahilbhende25 <sahilbhende25@gmail.com>
2026-07-31 19:58:42 -0400

    [20260726]-POTD

---
+++ POTD/877.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+
+    int solve(vector<int>&nums, int i , int j) {
+        if(i==j) return nums[i];
+        if(dp[i][j] != -1) return dp[i][j];
+        int takeLeft = nums[i] - solve(nums,i+1,j);
+        int takeRight = nums[j] - solve(nums, i, j-1);
+        return dp[i][j] = max(takeLeft,takeRight);
+    }
+
+    bool stoneGame(vector<int>& nums) {
+        int n = nums.size();
+        dp.assign(n,vector<int>(n,-1));
+        return solve(nums,0,n-1) >= 0;
+    }
+};
