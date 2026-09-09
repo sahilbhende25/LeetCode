d70689665e34fc7c022e2f0d8b0de449a6804332
sahilbhende25 <sahilbhende25@gmail.com>
2026-09-09 00:43:08 -0400

    [20260908]-POTD

--- POTD/74.cpp
+++ POTD/74.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(),
-			cols = matrix[0].size(),
-            row = 0, col = cols - 1;
-			
-        while (row < rows && col > -1) {
-            int cur = matrix[row][col];
-            if (cur == target) return true;
-            if (target > cur) row++;
-            else col--;
+    bool binarySearch(vector<vector<int>>& matrix, int target, int row) {
+        int n = matrix[0].size();
+        int st = 0, end = n - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (target == matrix[row][mid])
+                return true;
+            else if (target > matrix[row][mid])
+                st = mid + 1;
+            else
+                end = mid - 1;
         }
-        
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int sRow = 0, eRow = m - 1;
+
+        while (sRow <= eRow) {
+            int mRow = sRow + (eRow - sRow) / 2;
+
+            if (target >= matrix[mRow][0] && target <= matrix[mRow][n - 1]) {
+                return binarySearch(matrix, target, mRow);
+            } else if (target >= matrix[mRow][n - 1]) {
+                sRow = mRow + 1;
+            } else {
+                eRow = mRow - 1;
+            }
+        }
         return false;
     }
 };
